be22f9c7fdcbe96faf4dce5323e9806651582ac3
vinson <vinson.t@agiletechnica.com>
2022-03-28 21:48:27 +0700

    add apache data and show table

--- skripsi-2/src/App.js
+++ skripsi-2/src/App.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 
 import './App.css';
 import BarChart from './components/BarChart';
-import {AppData} from './Data.js';
+import { AppData } from './Data.js';
+import { ApacheData } from './ApacheData.js';
 
 function App() {
   const [appData, setAppData] = useState({
@@ -11,13 +12,26 @@ function App() {
       {
         label: "app",
         data: AppData.map((data) => data.jumlah),
+        backgroundColor: ["orange"]
+      },
+    ],
+  });
+
+  const [apacheData, setApacheData] = useState({
+    labels: ApacheData.map((data) => data.info),
+    datasets: [
+      {
+        label: "app",
+        data: ApacheData.map((data) => data.jumlah),
       },
     ],
   });
 
   return (
     <div className='App'>
-      <BarChart chartData={appData}/>
+      <BarChart chartData={appData} />
+      <BarChart chartData={apacheData} />
+
     </div>
   );
 }

7818f5d36e5fd5ff8e444b7d1f3ebe20aef044b4
vinson <vinson.t@agiletechnica.com>
2022-04-26 21:16:51 +0700

    pemindahan file dan pembuatan tabel

--- skripsi-2/src/App.js
+++ skripsi-2/src/App.js
@@ -1,5 +1,3 @@
-import React, { useState } from 'react'
-
 import './App.css';
 import BarChart from './components/BarChart';
 import { AppData } from './Data.js';
@@ -12,6 +10,8 @@ import { JQueryData } from './JQueryData';
 
 import BasicTable from './components/Table/BasicTable';
 import UrlNumsites from "./json/url_all_result_table.json";
+import DataUnsupportedJson from "./json/category_per_jumlah_unsupported.json";
+import DataNumsites from "./json/numsites_app_result.json";
 
 //Create Table with JSON data
 const columnsUrl = [
@@ -42,11 +42,71 @@ const columnsUrl = [
   },
 ];
 
+const columnsUnsupported = [
+  {
+    Header: "Number of Categories by Unsupported Version",
+    columns: [
+      {
+        Header: "Number of Unsupported = 0",
+        accessor: "n0",
+      },
+      {
+        Header: "Number of Unsupported = 1",
+        accessor: "n1",
+      },
+      {
+        Header: "Number of Unsupported = 2",
+        accessor: "n2",
+      },
+      {
+        Header: "Number of Unsupported = 3",
+        accessor: "n3",
+      },
+      {
+        Header: "Number of Unsupported >= 4",
+        accessor: "n4",
+      },
+    ],
+  },
+];
+
+const columnsNumsites = [
+  {
+    Header: "Top 10 popular technologies",
+    columns: [
+      {
+        Header: "Num Sites",
+        accessor: "num_sites",
+      },
+      {
+        Header: "App",
+        accessor: "app",
+      },
+      {
+        Header: "Supported",
+        accessor: "supported",
+      },
+      {
+        Header: "Unsupported",
+        accessor: "unsupported",
+      },
+      {
+        Header: "Not Versioned",
+        accessor: "not_versioned",
+      },
+      {
+        Header: "Non Conclusive",
+        accessor: "non_conclusive",
+      },
+    ],
+  },
+];
+
 //Create Measurement Color
 function color(arr) {
   let temp = [];
   for (let i = 0; i < arr.length; i++) {
-    arr[i].result === "UNSUPPORTED" ? color.push("red") : color.push("#0000FF"); //biru
+    arr[i].result === "UNSUPPORTED" ? temp.push("red") : temp.push("#0000FF"); //biru
   }
   return temp
 }
@@ -132,6 +192,8 @@ function App() {
 
   return (
     <div className='App'>
+      <BasicTable data={DataUnsupportedJson} columns={columnsUnsupported} />
+      <BasicTable data={DataNumsites} columns={columnsNumsites} />
       <BasicTable data={UrlNumsites} columns={columnsUrl} />
       <BarChart chartData={appData} />
       <BarChart chartData={apacheData} />

923b801dd557236095288fc08abd96e9f4c5d95c
vinson <vinson.t@agiletechnica.com>
2022-05-04 23:07:45 +0700

    revisi format folder

--- skripsi-2/src/components/Table/BasicTable.js
+++ skripsi-2/src/components/Table/BasicTable.js
@@ -1,30 +1,26 @@
-import React from 'react'
+import React from "react";
 import "./table.css";
 
 function BasicTable({ data, title }) {
-
   return (
     <div className="tabel-data">
-      <p>
-        {title}
-      </p>
+      <p>{title}</p>
       <table className="table">
         <thead>
           <tr>
-            {Object.keys(data[0]).map((value, key) =>
-              <th key={key}>
-                {value}
-              </th>)}
+            {Object.keys(data[0]).map((value, key) => (
+              <th key={key}>{value}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          {data.map((value, key) =>
+          {data.map((value, key) => (
             <tr key={key}>
-              {Object.values(value).map((items, _key) =>
-                <td key={_key}>
-                  {items}
-                </td>)}
-            </tr>)}
+              {Object.values(value).map((items, _key) => (
+                <td key={_key}>{items}</td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
--- skripsi-2/src/data/CategoryPerJumlahUnsupported.js
+++ skripsi-2/src/data/CategoryPerJumlahUnsupported.js
@@ -1,10 +1,9 @@
-export const CATEGORY_PER_UNSUPPORTED = 
-[
-    {
-      "n0": "2",
-      "n1": "1",
-      "n2": "0",
-      "n3": "0",
-      "n4": "58"
-    }
-  ]
+export const CATEGORY_PER_UNSUPPORTED = [
+  {
+    n0: "2",
+    n1: "1",
+    n2: "0",
+    n3: "0",
+    n4: "58",
+  },
+];
